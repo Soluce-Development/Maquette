41633d1130b460503f7c812e8314ca436394b105
Killian Larcher <killian.larcher@2024.icam.fr>
2022-11-12 12:34:02 +0100

    modify EmergencyStop

--- Models/ScreenModels.py
+++ Models/ScreenModels.py
@@ -90,6 +90,7 @@ class ProgramsList(QMainWindow, Screen):
         self.btn_start.clicked.connect(self.handle_navigation)
 
         self.program_chosen = False
+        self.text_choose_progam.setText("")
 
     def handle_navigation(self):
         if self.program_chosen:
