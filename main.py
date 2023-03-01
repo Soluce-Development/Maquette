545b016018312fdd37d47803729ec90dc97e44bd
Killian Larcher <killian.larcher@2024.icam.fr>
2023-03-01 16:50:19 +0100

    change GPIO

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 
+import GPIO as GPIO
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QCursor
 from PyQt5.QtWidgets import QApplication
 
+from Constants import SENSOR_PEDAL
 from Controllers.EventsHandler import initial_events, add_events
 from Models.ScreenModels import MainScreen
 
@@ -16,4 +18,5 @@ if __name__ == "__main__":
     main_screen.show()
     initial_events()
     add_events()
+    GPIO.remove_event_detect(SENSOR_PEDAL)
     sys.exit(app.exec_())
