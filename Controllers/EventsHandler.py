e85e994c59e273b7e0a6e974cb04689e7b22ece0
Killian Larcher <killian.larcher@2024.icam.fr>
2023-03-01 10:51:40 +0100

    change GPIO

--- Controllers/EventsHandler.py
+++ Controllers/EventsHandler.py
@@ -9,7 +9,7 @@ INPUTS = (SENSOR_DOOR, BTN_DOOR, BTN_START, BTN_EMERGENCY, SENSOR_PEDAL)
 GPIO.setup(INPUTS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Setting Outputs
-OUTPUTS = (LED_MACHINING,)
+OUTPUTS = (LED_MACHINING, LED_EMERGENCY)
 # OUTPUTS = (ACTUATOR_DOOR, ACTUATOR_JAW, LED_DOOR, LED_MACHINING)
 GPIO.setup(OUTPUTS, GPIO.OUT, initial=GPIO.LOW)
 
@@ -33,6 +33,14 @@ def toggle_led_door(pin):
         GPIO.output(LED_DOOR, GPIO.LOW)
 
 
+def toggle_led_emergency(pin):
+    """Turn on/off the LED_DOOR."""
+    if not GPIO.input(BTN_EMERGENCY):
+        GPIO.output(LED_EMERGENCY, GPIO.HIGH)
+    else:
+        GPIO.output(LED_EMERGENCY, GPIO.LOW)
+
+
 def toggle_jaw(pin):
     """Open/close the jaw."""
     GPIO.output(ACTUATOR_JAW, not GPIO.input(ACTUATOR_JAW))
@@ -41,14 +49,16 @@ def toggle_jaw(pin):
 # --- Add events ---
 
 def initial_events():
-    pass
+    # pass
     # open_door(BTN_DOOR)
     # toggle_led_door(SENSOR_DOOR)
     # toggle_jaw(SENSOR_PEDAL)
+    toggle_led_emergency(BTN_EMERGENCY)
 
 
 def add_events():
     pass
     # GPIO.add_event_detect(BTN_DOOR, GPIO.BOTH, callback=open_door)
+    GPIO.add_event_detect(BTN_EMERGENCY, GPIO.BOTH, callback=toggle_led_emergency)
     # GPIO.add_event_detect(SENSOR_DOOR, GPIO.BOTH, callback=toggle_led_door, bouncetime=10)
     # GPIO.add_event_detect(SENSOR_PEDAL, GPIO.RISING, callback=toggle_jaw, bouncetime=100)
